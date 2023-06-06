b3daa6d985af12fb9a71704f3a18d4ae7d6950b7
jwsmin <43048181+jwsmin@users.noreply.github.com>
2023-06-06 15:58:11 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    //todo:
+    /*
+        5 options for users
+        check balance
+        withdraw
+        deposit
+        check previous transaction
+        exit
+
+
+     */
+}
